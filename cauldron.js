dafeadb1a9d9bcdac50655bf5908b23b2c82c65e
FairTex <makarov906@gmail.com>
2015-12-08 02:55:06 +0500

    1st
    -перетаскивание в котел

--- cauldron.js
+++ cauldron.js
@@ -3,4 +3,35 @@
 // Получаем комбинации элементов
 var formulas = window.formulas;
 
-// Механизмы котла описывать здесь
+var outOfBoiler = document.getElementById("outOfBoiler");
+var inBoiler = document.getElementById("inBoiler");
+
+for (var li of [].slice.call(outOfBoiler.children)) {
+    li.onclick = toBoiler.bind(li);
+}
+
+for (var li of [].slice.call(inBoiler.children)) {
+    li.onclick = toBoilerOut.bind(li);
+}
+
+function toBoiler(li) {
+    inBoiler.appendChild(this);
+    checkBoiler();
+    this.onclick = toBoilerOut.bind(this);
+}
+
+function toBoilerOut(li) {
+    outOfBoiler.appendChild(this);
+    checkBoiler();
+    this.onclick = toBoiler.bind(this);
+}
+
+function checkBoiler() {
+    for (var formula of formulas) {
+        for (var li of [].slice.call(inBoiler.children)) {
+
+        }
+    }
+
+}
+
